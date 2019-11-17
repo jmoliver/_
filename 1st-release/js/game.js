78e972271241bec310d16828c9dd488bcdd38ef7
Jean Michel <jeanmicheloliver@hotmail.com>
2019-11-17 15:21:16 +0000

    Separa as camadas da arquitetura em arquivos distintos

---
+++ 1st-release/js/game.js
@@ -0,0 +1,47 @@
+export function createGame() {
+  const state = {
+    players: {
+      player1: { x: 1, y: 1 },
+      player2: { x: 9, y: 9 }
+    },
+    fruits: {
+      fruit1: { x: 3, y: 1 }
+    },
+    screen: {
+        width: 10,
+        height: 10
+    }
+  };
+
+  function movePlayer(command) {
+    console.log(`Moving ${command.playerId} with ${command.keyPressed}`);
+
+    const keyPressed = command.keyPressed;
+    const player = state.players[command.playerId];
+
+    if (keyPressed === 'ArrowUp' && player.y > 0) {
+      player.y -= 1;
+      return;
+    }
+
+    if (keyPressed === 'ArrowRight' && (player.x + 1) < state.screen.height) {
+      player.x += 1;
+      return;
+    }
+
+    if (keyPressed === 'ArrowDown' && (player.y + 1) < state.screen.width) {
+      player.y += 1;
+      return;
+    }
+
+    if (keyPressed === 'ArrowLeft' && player.x > 0) {
+      player.x -= 1;
+      return;
+    }
+  }
+
+  return {
+    movePlayer: movePlayer,
+    state: state
+  };
+}
---
+++ 1st-release/js/view.js
@@ -0,0 +1,30 @@
+import { createGame } from './game.js';
+import { createKeyboardListener } from './input.js';
+
+const screen = document.querySelector('#screen');
+const context = screen.getContext('2d');
+const currentPlayerId = 'player1';
+const game = createGame();
+const keyboardListener = createKeyboardListener();
+
+keyboardListener.subscribe(game.movePlayer);
+
+renderScreen();
+
+function renderScreen() {
+  context.clearRect(0, 0, game.state.screen.width, game.state.screen.height);
+
+  for (const playerId in game.state.players) {
+    const player = game.state.players[playerId];
+    context.fillStyle = 'black';
+    context.fillRect(player.x, player.y, 1, 1);
+  }
+
+  for (const fruitId in game.state.fruits) {
+    const fruit = game.state.fruits[fruitId];
+    context.fillStyle = 'green';
+    context.fillRect(fruit.x, fruit.y, 1, 1);
+  }
+
+  requestAnimationFrame(renderScreen);
+}
